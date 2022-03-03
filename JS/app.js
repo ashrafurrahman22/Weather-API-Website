3645002d7dbac51fb7d65e379fc8f66da185b796
ashrafurrahman22 <ashrafurrahman22@gmail.com>
2022-03-03 09:56:43 +0600

    last

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,26 @@
 const API_KEYS =`17beec452dca631d3de3c15c416fa5fd`
 
+
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEYS}&units=metric`;
     // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+     setInnerText('city', temperature.name)
+     setInnerText('temperature', temperature.main.temp);
+     setInnerText('condition', temperature.weather[0].main);
+    // console.log(temperature);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon'); 
+    imgIcon.setAttribute('src', url);
 }
